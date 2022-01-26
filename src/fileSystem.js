a053d347a5b735ed5250fbf733c4e1ef1ed252c7
C Bares <colinedwinbares@gmail.com>
2022-01-25 21:29:01 -0600

    set up fileSystem.js to get data, generate html, then output file to dist folder

--- src/fileSystem.js
+++ src/fileSystem.js
@@ -1,3 +1,15 @@
 const fs = require('fs');
 const html = require('./html');
 
+const file = {
+
+    async createHTML() {
+        let htmlString = await html.buildPage();
+        fs.writeFile('./dist/index.html', htmlString, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        })
+    }
+}
+
+file.createHTML();
